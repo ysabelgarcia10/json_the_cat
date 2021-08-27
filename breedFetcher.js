06dce6d03b06de665d680a193ee4d8aa2886b823
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-27 00:51:31 +0000

    tested with mocha and chai: w02d4 assignment - testing cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,27 @@
 const request = require('request');
-const input = process.argv.slice(2);
-const inputNoSpace = input.join('_');
-const path = 'https://api.thecatapi.com/v1/breeds/search?q=' + inputNoSpace;
 
-request(path, (error, response, body) => {
-  //finds error first such as typo in URL
-  if (error) {
-    console.log("ERROR FOUND!!!");
-    throw error;
-  }
+
+const fetchBreedDescription = function(breedName, callback) {
+  //URL path name
+  const path = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+  request(path, (error, response, body) => {
+    //finds error first such as typo in URL
+    if (error) {
+      callback(error, null);
+    }
+    
+    //handles data, if data doesn't exist
+    const dataBody = JSON.parse(body);
+    if (dataBody.length === 0) {
+      callback("Please enter a valid cat breed.", null);
+    
+    //if data is found or exists:
+    } else {
+      callback(null, dataBody[0].description);
+    }
+  });
+};
   
-  //handles data
-  const dataBody = JSON.parse(body);
-  if (dataBody.length === 0) {
-    console.log("Please enter a valid cat breed.");
-  } else {
-    console.log(dataBody[0].description);
-  }
-});
-  
+
+module.exports = { fetchBreedDescription };
